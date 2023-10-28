267b2550f0e3457737be3bcacfe6123fa97abb2c
Bipin Gosain <bipin_gosain@epam.com>
2023-10-28 11:23:20 +0530

    Added mock test for createInvalidRequest

--- import-service/src/constants/response.js
+++ import-service/src/constants/response.js
@@ -1,3 +1,4 @@
 module.exports = {
   SUCCESS: "Request processed successfully!",
+  INVALID_REQUEST: "Invalid Request",
 };
--- import-service/src/handlers/importProductsFile/index.js
+++ import-service/src/handlers/importProductsFile/index.js
@@ -3,15 +3,27 @@ const { PutObjectCommand } = require("@aws-sdk/client-s3");
 
 const { client } = require("../../clients/s3Client");
 const createSuccessReponse = require("../../common/createSuccessReponse");
-const { SUCCESS } = require("../../constants/response");
+const { SUCCESS, INVALID_REQUEST } = require("../../constants/response");
+const createInvalidRequest = require("../../common/createInvalidRequest");
 
 const UPLOAD_BUCKET = process.env.BUCKET_NAME;
 
+const returnInvalidRequest = () => {
+  return createInvalidRequest(
+    INVALID_REQUEST,
+    new Error('Filename missing as "name" parameter'),
+    400
+  );
+};
+
 module.exports.handler = async (event) => {
-  console.log("Validate the request...");
-  const {
-    queryStringParameters: { name },
-  } = event;
+  const { queryStringParameters } = event;
+
+  if (!queryStringParameters) return returnInvalidRequest();
+
+  const { name } = queryStringParameters;
+
+  if (!name) return returnInvalidRequest();
 
   console.log(`Received filename is : '${name}'`);
 
