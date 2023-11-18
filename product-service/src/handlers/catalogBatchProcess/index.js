63dcf0976a8201c78d47fd398e967dee1c430dcc
Bipin Gosain <bipin_gosain@epam.com>
2023-11-18 06:36:17 +0530

    Complete with adding filter and tests for catalogBatchProcess

--- product-service/src/handlers/catalogBatchProcess/index.js
+++ product-service/src/handlers/catalogBatchProcess/index.js
@@ -4,11 +4,27 @@ const { createProduct } = require("../../database/productTable");
 module.exports.handler = async (event) => {
   const { SNS_ARN } = process.env;
   const { Records } = event;
-  for (const record of Records) {
-    const products = JSON.parse(record.body);
-    for (const product of products) {
-      await createProduct(product);
-    }
+
+  if (Records.length <= 0) {
+    console.log("No data in the records!");
+    return;
   }
-  await publishToSNS(`Products added to dynamoDB`, SNS_ARN);
+  try {
+    let maxPrice = Number.MIN_VALUE;
+
+    for (const record of Records) {
+      const products = JSON.parse(record.body);
+      for (const product of products) {
+        const { price } = product;
+        if (+price > maxPrice) {
+          maxPrice = +price;
+        }
+        await createProduct(product);
+      }
+    }
+    console.log(maxPrice);
+    await publishToSNS(`Products added to dynamoDB`, SNS_ARN, maxPrice);
+  } catch (e) {
+    console.log("Error while saving records!");
+  }
 };
