c8bd01a1634711f3d52d120ccdb3663230c8f028
Bipin Gosain <bipin_gosain@epam.com>
2023-11-18 06:36:17 +0530

    Complete with adding filter and tests for catalogBatchProcess

--- product-service/src/clients/snsClient.js
+++ product-service/src/clients/snsClient.js
@@ -1,14 +1,22 @@
-import { SNSClient } from "@aws-sdk/client-sns";
-import { PublishCommand } from "@aws-sdk/client-sns";
+const { SNSClient } = require("@aws-sdk/client-sns");
+const { PublishCommand } = require("@aws-sdk/client-sns");
 
 const snsClient = new SNSClient({ region: process.env.AWS_REGION });
 
-export const publishToSNS = async (message, topicArn) => {
+const publishToSNS = async (message, topicArn, maxPrice) => {
   const response = await snsClient.send(
     new PublishCommand({
       Message: message,
       TopicArn: topicArn,
+      MessageAttributes: {
+        maxPrice: {
+          DataType: "Number",
+          StringValue: `${maxPrice}`,
+        },
+      },
     })
   );
   return response;
 };
+
+module.exports = { publishToSNS };
